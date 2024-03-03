ee31881d3b6cea509feaf3a5a02ab830e675ce4e
princey6sams <princeasingha@gmail.com>
2024-03-02 23:51:48 -0330

    Added DP Review link and getting data for camera.

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from bs4 import BeautifulSoup
 import requests
-import smtplib
+import smtplib # For sending emails to yourself
 import time
 import datetime
+
+# Connect to the website
+url = 'https://www.dpreview.com/reviews/nikon-zf-full-frame-mirrorless-camera-review'
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:123.0) Gecko/20100101 Firefox/123.0"}
+
+page = requests.get(url, headers=headers)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+soup2 = soup.prettify()
+print (soup2)
