9d8e2c9f7ff4d470f9e910e4a62bdb01a6db60a8
princey6sams <princeasingha@gmail.com>
2024-03-02 23:59:30 -0330

    Changed to Nikon Website, but having trouble with ids...

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ import smtplib # For sending emails to yourself
 import time
 import datetime
 
-# Connect to the website
-url = 'https://www.dpreview.com/reviews/nikon-zf-full-frame-mirrorless-camera-review'
+# Connect to the website (Multiple website to automate a data collection thing to run periodically and update the price and/or avalilability of a product?)
+url = 'https://store.nikon.ca/nikon-products/product/mirrorless-cameras/z-f.html'
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:123.0) Gecko/20100101 Firefox/123.0"}
 
 page = requests.get(url, headers=headers)
 
 soup = BeautifulSoup(page.content, 'html.parser')
 soup2 = soup.prettify()
-print (soup2)
+title = soup2.find().get_text()
+print (title)
